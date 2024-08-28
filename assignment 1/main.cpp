d787783b7928fbced882c2bccacad4288e4d7478
strmfly <maya.n102304@gmail.com>
2024-08-28 15:34:27 -0700

    Initial Commit

---
+++ assignment 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  assignment 1
+//
+//  Created by maya nachiappan on 8/28/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
