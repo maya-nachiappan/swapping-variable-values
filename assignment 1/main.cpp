897377acb7a26d7eecd0b9345d8f92c8393340f6
strmfly <maya.n102304@gmail.com>
2024-08-28 22:46:35 -0700

    final

--- assignment 1/main.cpp
+++ assignment 1/main.cpp
@@ -12,11 +12,11 @@ int main()
 {
     int num1=2;
     int num2=1;
-    cout << "Hello\n" << "Num 1 is: "<< num1 << " and Num 2 is: " << num2 << endl;
+    cout << "Hello!\n" << "Num 1 is: "<< num1 << " and Num 2 is: " << num2 << endl;
     int temp; // initializing a temp variable to swap the numbers
     temp = num1;
     num1 = num2;
     num2 = temp ;
-    cout <<"The new values are now Num 1: " << num1<< " and Num 2: " <<num2 << endl;
+    cout <<"After swapping with a temp variable, the new values are now Num 1: " << num1<< " and Num 2: " <<num2 << endl;
     return 0;
 }
